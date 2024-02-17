c935709b637e92a2acb633873e1300bfd30671ef
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-02-16 20:22:34 -0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -232,7 +232,7 @@ void MainWindow::on_actionBoot_Game_triggered()
 
     QString firmwareDirectory = settings.value("FirmwareDirectory").toString();
     QString chosenGame = settings.value("ChosenGame").toString();
-    QString fpsSlider = settings.value("FPSSlider").toString();
+    QString fpsSlider = settings.value("FPSslider").toString();
     QString hudDisplay = settings.value("HUDdisplay").toString();
 
     QString command = QString("rm -f /dev/shm/rpcsx-* && MANGOHUD=1 MANGOHUD_CONFIG=\"fps_limit=%3,%4\" rpcsx-os --mount \"%1\" /system --mount \"%2\" /app0 /app0/eboot.bin").arg(firmwareDirectory, chosenGame, fpsSlider, hudDisplay);
