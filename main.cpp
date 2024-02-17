dfc13b22eef9ada36f6f58a0c0762657c58eec5f
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-02-16 19:38:53 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
 	MainWindow window;
+	
+	QSettings settings("rpcsx", "rpcsx_ui_settings");
+    settings.beginGroup("rpcsx_ui_settings");
+	QObject::connect(&app, &QApplication::aboutToQuit, [&]() {
+		int windowHeight = window.height();
+    	int windowWidth = window.width();
+        settings.setValue("windowWidth", windowWidth);
+        settings.setValue("windowHeight", windowHeight);
+    });
+    
 	window.show();
 
     return app.exec();
